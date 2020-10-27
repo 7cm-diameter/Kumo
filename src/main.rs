a3579c25babff0f43adfa4affc3fdc014e66a530
RidiculousLoop <from.this.ridiculous.loop@gmail.com>
2020-10-28 07:36:57 +0900

    Add support for passing command line argument.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 extern crate yup_oauth2 as oauth2;
 
 use kumo::gdrive::{api, app::GoogleDriveClient};
+use std::env::args;
 
 const SCOPES: &[&str] = &[
   "https://www.googleapis.com/auth/drive",
@@ -10,20 +11,47 @@ const SCOPES: &[&str] = &[
 
 #[tokio::main]
 async fn main() {
+  let clargs: Vec<String> = args().collect();
+
   let app = GoogleDriveClient::default(SCOPES).await;
 
-  let x = app
-    .files_list(
-      api::files::FilesListQuery::default()
-        .set_page_size(50)
-        .include_items_form_all_drives(true)
-        .set_order(api::files::Order::ModifiedTime),
-    )
-    .await;
+  if let Some(first_arg) = clargs.get(1) {
+    match first_arg.as_str() {
+      "ls" => {
+        let x = app
+          .files_list(
+            api::files::FilesListQuery::default()
+              .set_page_size(50)
+              .include_items_form_all_drives(true)
+              .set_order(api::files::Order::ModifiedTime),
+          )
+          .await;
 
-  x.files.iter().for_each(|f: &api::files::File| {
-    println!("{:?}: {:?}", &f.name.as_ref(), &f.id.as_ref());
-  });
-
-  app.fetch_file(&x.files[2], None, None).await;
+        x.files
+          .iter()
+          .enumerate()
+          .for_each(|(i, f)| println!("{:?}: {:?}", &i, &f.name));
+      }
+      "fetch" => {
+        let x = app
+          .files_list(
+            api::files::FilesListQuery::default()
+              .set_page_size(50)
+              .include_items_form_all_drives(true)
+              .set_order(api::files::Order::ModifiedTime),
+          )
+          .await;
+        let n = if let Some(n) = clargs.get(2) {
+          n.parse().unwrap_or_else(|_| 0)
+        } else {
+          0
+        };
+        app.fetch_file(&x.files[n], None, None).await;
+      }
+      "upload" => println!("Not implemented yet."),
+      _ => println!("Command {:} does not exist.", first_arg),
+    }
+  } else {
+    println!("No command will be executed.");
+  }
 }
